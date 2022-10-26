a0f5c38f507eb6b1a5956b2573411f9eae10a849
Anas <anasmahmud523@gmail.com>
2022-10-26 20:16:44 +0600

    course id

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const corses = require('./data/course.json')
+const courses = require('./data/course.json')
 
 app.get('/', (req, res) => [
     res.send('api Running')
 ])
 
-app.get('/courses-data', (req, res) => {
-    res.send(corses)
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCourse = courses.find(course => course.id == id);
+    res.send(singleCourse)
 })
 
 app.listen(port, () => {
