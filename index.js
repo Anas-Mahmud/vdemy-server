781a42a16014610eceafda0e50fb563fbb96b0f6
Anas <anasmahmud523@gmail.com>
2022-10-26 15:59:08 +0600

    edit data

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
-const categories = require('./data/course.json')
+const corses = require('./data/course.json')
 
 app.get('/', (req, res) => [
     res.send('api Running')
 ])
 
 app.get('/courses-data', (req, res) => {
-    res.send(categories)
+    res.send(corses)
 })
 
 app.listen(port, () => {
