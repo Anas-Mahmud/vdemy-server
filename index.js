8dbd4532ca92f7de7d3344e9f84f5018e1010459
Anas <anasmahmud523@gmail.com>
2022-10-27 15:30:54 +0600

    done

--- index.js
+++ index.js
@@ -4,25 +4,24 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
-const courses = require('./data/course.json')
+const courses = require('./data/course.json');
 
 app.get('/', (req, res) => [
     res.send('api Running')
-])
+]);
 
 app.get('/courses', (req, res) => {
     res.send(courses)
-})
+});
 
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const singleCourse = courses.find(course => course.id == id);
     res.send(singleCourse)
-})
+});
 
 app.listen(port, () => {
     console.log('server is running on port', port);
-})
+});
 
 module.exports = app;
